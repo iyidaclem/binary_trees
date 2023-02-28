193fce2c993cae1754b63d874c0268fd7f728a03
iyidaclem <iyidaclem@gmail.com>
2023-02-28 17:45:14 +0100

    Creating binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -5,11 +5,7 @@
 #include <stddef.h>
 
 /**
-* Basic Binary Tree
-*/
-/**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -17,36 +13,36 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
-/**
+/*
 * Binary Search Tree
 */
 typedef struct binary_tree_s bst_t;
 
-/**
+/*
 * AVL Tree
 */
 typedef struct binary_tree_s avl_t;
 
-/**
+/*
 * Max Binary Heap
 */
 typedef struct binary_tree_s heap_t;
 
-/**
+/*
 * Printing of the binary tree
 */
 
 void binary_tree_print(const binary_tree_t *tree);
 
-/**
+/*
 *  Main functions
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
