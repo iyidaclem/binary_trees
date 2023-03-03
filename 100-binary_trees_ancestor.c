ae36d983d64a7f9d0bf9fac08abc50c2fa10bcaa
iyidaclem <iyidaclem@gmail.com>
2023-03-03 14:11:24 +0100

    Function to detect common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,64 +1,26 @@
 #include "binary_trees.h"
-/**
-* binary_tree_depth - Function to return the depth of a node
-* @tree: pointer to the node
-* Return: depth of the node
-*/
-
-
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	if (tree == NULL || tree->parent == NULL)
-		return (0);
-	return (binary_tree_depth(tree->parent) + 1);
-}
-
 
 /**
-* binary_trees_ancestor - lowest common ancestor of two nodes
-* @first: The first node
-* @second: The second node
-* Return: Pointer to the ancestor or NULL if not exists
-*/
-
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
+ *
+ * Return: If no common ancestors return NULL, else return common ancestor.
+ */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-	const binary_tree_t *second)
+		const binary_tree_t *second)
 {
-	size_t fDepth = 0, sDepth =  0, diff =  0;
+	binary_tree_t *mom, *pop;
 
-	if (first != NULL && second != NULL)
-	{
-		fDepth = binary_tree_depth(first);
-		sDepth = binary_tree_depth(second);
-		if (fDepth == sDepth)
-		{
-			if (first->parent == second->parent)
-				return (first->parent);
-			return (binary_trees_ancestor(first->parent, second->parent));
-		}
-		else if (fDepth > sDepth)
-		{
-			diff = fDepth - sDepth;
-			for (; diff > 0; diff--)
-			{
-				if (first->parent == second)
-					return (first->parent);
-				first = first->parent;
-			}
-			return (binary_trees_ancestor(first, second));
-		}
-		else
-		{
-			diff = sDepth - fDepth;
-			for (; diff > 0; diff--)
-			{
-				if (second->parent == first)
-					return (second->parent);
-				second = second->parent;
-			}
-			return (binary_trees_ancestor(first, second));
-		}
-	}
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-	return (NULL);
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
